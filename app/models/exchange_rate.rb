50a5511e8a2ff17777248cc20e87e9d9b4f44288
portmare <portmare@gmail.com>
2018-03-25 17:56:48 +0400

    Добавлены метод получения актуального списка курсов валют

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -2,4 +2,13 @@ class ExchangeRate < ApplicationRecord
   enum currency: { usd: 'usd' }
 
   validates :rate, :expired_at, :currency, presence: true
+
+  scope :active, -> { where('expired_at > ?', Time.zone.now) }
+  scope :group_by_currency, -> { select('DISTINCT ON (currency) id, currency, rate').order(currency: :desc, id: :desc) }
+
+  def self.current_rates
+    CBR::Client.last_curses.deep_merge(
+      Hash[active.group_by_currency.map { |x| [x.currency, { rate: x.rate }] }]
+    )
+  end
 end
--- spec/models/exchange_rate_spec.rb
+++ spec/models/exchange_rate_spec.rb
@@ -1,23 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe ExchangeRate, type: :model do
-  it 'is valid with currency, rate and expired_at' do
-    exchange_rate = build(:exchange_rate)
-    expect(exchange_rate).to be_valid
+  context 'validation' do
+    it 'is valid with currency, rate and expired_at' do
+      exchange_rate = build(:exchange_rate)
+      expect(exchange_rate).to be_valid
+    end
+
+    it 'is not valid without currency' do
+      exchange_rate = build(:exchange_rate, currency: nil)
+      expect(exchange_rate).to be_invalid
+    end
+
+    it 'is not valid without rate' do
+      exchange_rate = build(:exchange_rate, rate: nil)
+      expect(exchange_rate).to be_invalid
+    end
+
+    it 'is not valid without expired_at' do
+      exchange_rate = build(:exchange_rate, expired_at: nil)
+      expect(exchange_rate).to be_invalid
+    end
   end
 
-  it 'is not valid without currency' do
-    exchange_rate = build(:exchange_rate, currency: nil)
-    expect(exchange_rate).to be_invalid
-  end
+  context 'class methods' do
+    context 'active' do
+      before :each do
+        create(:exchange_rate, expired_at: Time.now - 1.seconds)
+      end
 
-  it 'is not valid without rate' do
-    exchange_rate = build(:exchange_rate, rate: nil)
-    expect(exchange_rate).to be_invalid
-  end
+      it 'return array of exchange rates with expired_at > Time.now' do
+        exchange_rate = create(:exchange_rate)
+        expect(described_class.active).to match_array([exchange_rate])
+      end
 
-  it 'is not valid without expired_at' do
-    exchange_rate = build(:exchange_rate, expired_at: nil)
-    expect(exchange_rate).to be_invalid
+      it 'return empty array with expired_at < Time.now' do
+        expect(described_class.active).to eq []
+      end
+    end
+
+    context 'group_by_currency' do
+      before :each do
+        create_list(:exchange_rate, 5)
+      end
+
+      it 'return last rates for each currency' do
+        rate_usd = create(:exchange_rate, rate: 10_000)
+        expect(described_class.group_by_currency).to match_array([rate_usd])
+      end
+    end
+
+    context 'current_rates' do
+      it 'return CBR rate for USD with expired_at < Time.now' do
+        create(:exchange_rate, expired_at: Time.now - 1.seconds, rate: 10_000)
+        expect(described_class.current_rates[:usd][:rate]).not_to eq 10_000
+      end
+
+      it 'return DB rate for USD with expired_at > Time.now' do
+        create(:exchange_rate, rate: 10_000)
+        expect(described_class.current_rates[:usd][:rate]).to eq 10_000
+      end
+    end
   end
 end
