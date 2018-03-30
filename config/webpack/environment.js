55b347d49abceabfa07a1b2034623d53931ef87a
portmare <portmare@gmail.com>
2018-03-30 22:36:18 +0400

    Исправлено прохождение тестов request

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,12 @@
 const { environment } = require('@rails/webpacker')
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
